f18165536d0791ba3be4a8e8177edcf6c1d4bcd2
omar115 <omarhasan115@gmail.com>
2021-07-22 16:16:20 +0600

    variable types learn

--- basics/variable_list.go
+++ basics/variable_list.go
@@ -11,4 +11,18 @@ func var_list() {
 	var nameOne string = "Hi.. this is a string"
 	var nameTwo string = "Mario"
 	fmt.Println(nameOne, nameTwo)
+
+	// short from of declaring variables
+	nameFour := "youshi"
+	fmt.Println(nameFour)
+
+	// integers
+	nameFive := 20898908908908
+	fmt.Println(nameFive)
+
+	// float
+	nameSix := 87894738497389473984.38938
+
+	fmt.Println(nameSix)
+
 }
