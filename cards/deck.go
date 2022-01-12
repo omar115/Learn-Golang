84c1e1b7ccdf954ac3cbf52497d00b86388181ce
omar115 <omarhasan115@gmail.com>
2022-01-13 02:59:15 +0600

    starting-deck-project

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Carroms"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+}
+
+// d is the reference to deck, you can
+// write anything you want
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	cards := deck{"Hello", "Hello!"}
+	cards = append(cards, "Six of Spades")
 	fmt.Println(cards)
 
 	cards.print()
